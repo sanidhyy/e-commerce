785e586d94b4dae7c181256de6a35724f8b80332
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-02 06:22:01 +0000

    eslint EOF Error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
   root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
-}
+  extends: ["plugin:@next/next/recommended", "@payloadcms"],
+  ignorePatterns: ["**/payload-types.ts"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+};
