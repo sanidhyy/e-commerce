f95b10171e0621da2c81f93afc08bb7bd8c193c3
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-04 10:11:56 +0000

    categories section implemented

--- src/app/_components/CategoryCard/index.tsx
+++ src/app/_components/CategoryCard/index.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 
 import { Category, Media } from "../../../payload/payload-types";
+import { useFilter } from "../../_providers/Filter";
 
 import classes from "./index.module.scss";
 
@@ -10,11 +13,14 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media;
+  const { setCategoryFilters } = useFilter();
+
   return (
     <Link
       href="/products"
       className={classes.card}
       style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
