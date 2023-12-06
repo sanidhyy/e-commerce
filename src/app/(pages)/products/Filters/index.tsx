24ce7851e6a62e966c942dd1f792784df591ed7b
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2023-12-06 11:46:14 +0000

    filter functionality completed

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,5 +1,70 @@
-const Filters = () => {
-  return <div>Filters</div>;
+"use client";
+
+import { Category } from "../../../../payload/payload-types";
+import { Checkbox } from "../../../_components/Checkbox";
+import { HR } from "../../../_components/HR";
+import { RadioButton } from "../../../_components/RadioButton";
+import { useFilter } from "../../../_providers/Filter";
+
+import classes from "./index.module.scss";
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter();
+
+  const handleCategories = (categoryId: string) => {
+    if (categoryFilters.includes(categoryId)) {
+      const updatedCategories = categoryFilters.filter(
+        (id) => id !== categoryId
+      );
+
+      setCategoryFilters(updatedCategories);
+    } else {
+      setCategoryFilters([...categoryFilters, categoryId]);
+    }
+  };
+
+  const handleSort = (value: string) => {
+    setSort(value);
+  };
+
+  return (
+    <div className={classes.filters}>
+      <h6 className={classes.title}>Product Categories</h6>
+      <div className={classes.categories}>
+        {categories.map((category) => {
+          const isSelected = categoryFilters.includes(category.id);
+
+          return (
+            <Checkbox
+              key={category.id}
+              label={category.title}
+              value={category.id}
+              isSelected={isSelected}
+              onClickHandler={handleCategories}
+            />
+          );
+        })}
+      </div>
+      <HR className={classes.hr} />
+      <h6 className={classes.title}>Sort By</h6>
+      <div className={classes.categories}>
+        <RadioButton
+          label="Latest"
+          value="-createdAt"
+          isSelected={sort === "-createdAt"}
+          onRadioChange={handleSort}
+          groupName="sort"
+        />
+        <RadioButton
+          label="Oldest"
+          value="createdAt"
+          isSelected={sort === "createdAt"}
+          onRadioChange={handleSort}
+          groupName="sort"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Filters;
--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -31,7 +31,7 @@ const Products = async () => {
   return (
     <div className={classes.container}>
       <Gutter className={classes.products}>
-        <Filters />
+        <Filters categories={categories} />
         <Blocks blocks={page.layout} disableTopPadding />
       </Gutter>
       <HR />
